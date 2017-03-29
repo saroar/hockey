84ce76ec6af3b5da491c15a092a60939211a4a5d
Alif <saroar9@gmail.com>
2017-03-29 12:41:28 +0300

    refactor

--- format.rb
+++
@@ -1,5 +0,0 @@
-class Format
-  def initialize(filename)
-    @filename = filename
-  end
-end
--- lib/hockey.rb
+++ lib/hockey.rb
@@ -1,4 +1,6 @@
 class Hockey
+  attr_reader :team1_score, :team2_score, :filename
+
   def initialize(team1_score, team2_score, filename)
     @team1_score = team1_score
     @team2_score = team2_score
@@ -6,7 +8,7 @@ class Hockey
   end
 
   def valid_score?(score)
-    return true if score.is_a? Integer
+    score.is_a? Integer
   end
 
   def format
@@ -14,29 +16,30 @@ class Hockey
 
     return false unless valid_score?(@team1_score) && valid_score?(@team2_score)
 
-    if @filename == 'json'
-      match_result(result)
-      result.to_json
-    else
-      @filename == 'html'
-      match_result(result)
+    case filename
+      when 'json'
+        match_result(result)
+        result.to_json
+      when 'html'
+        filename == 'html'
+        match_result(result)
 
-      indent_level = result.fetch(:indent_level) { 0 }
+        indent_level = result.fetch(:indent_level, 0)
 
-      out = ' ' * indent_level + "<ul>\n"
+        out = ' ' * indent_level + "<ul>\n"
 
-      result.each do |key, value|
-        out += ' ' * (indent_level + 2) + "<li><strong>#{key}:</strong>"
-        out += " <span>#{value}</span></li>\n"
-      end
-      out += ' ' * indent_level + "</ul>\n"
+        result.each do |key, value|
+          out += ' ' * (indent_level + 2) + "<li><strong>#{key}:</strong>"
+          out += " <span>#{value}</span></li>\n"
+        end
+        out += ' ' * indent_level + "</ul>\n"
     end
   end
 
   def match_result(hash)
-    if @team1_score > @team2_score
+    if team1_score > team2_score
       hash.merge!(team1_score: 'Победила первая комадна')
-    elsif @team1_score < @team2_score
+    elsif team1_score < team2_score
       hash.merge!(team2_score: 'Победила вторая комадна')
     else
       hash.merge!(draw: 'Ничья')
